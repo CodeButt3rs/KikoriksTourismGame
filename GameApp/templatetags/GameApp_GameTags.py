4482734f2b281f8c629296e45c5f8201dd08b182
CodeButt3rs <tekrimon4ever@yandex.ru>
2022-02-16 01:31:51 +0500

    Шаблоны

--- GameApp/templatetags/GameApp_GameTags.py
+++ GameApp/templatetags/GameApp_GameTags.py
@@ -14,3 +14,23 @@ def getFatigueProgressBar(context):
 @register.inclusion_tag('GameApp/_inc/_gameContext.html',takes_context=True)
 def getGameContext(context):
     return context
+
+@register.inclusion_tag('GameApp/_inc/_attractionsCard.html',takes_context=True)
+def getAttractionsCard(context):
+    return context
+
+@register.inclusion_tag('GameApp/_inc/_attractionCard.html',takes_context=True)
+def getDayAttractions(context):
+    return context
+
+@register.inclusion_tag('GameApp/_inc/_stars.html', takes_context=False)
+def getAmountOfStars(k):
+    return {'k': k // 0.2}
+
+@register.inclusion_tag('GameApp/_inc/_antiFatigueBadge.html')
+def getAntiFatigueBadge(k):
+    return {'percents': int(abs((k.fatigue) * 100))}
+
+@register.inclusion_tag('GameApp/_inc/_FatigueBadge.html')
+def getFatigueBadge(k):
+    return {'percents': int(abs((k.fatigue) * 100))}
