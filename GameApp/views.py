acd0d8eb5ce409de08e74a99b886111b7208388f
CodeButt3rs <tekrimon4ever@yandex.ru>
2022-02-16 01:31:05 +0500

    Обработка формы

--- GameApp/views.py
+++ GameApp/views.py
@@ -1,9 +1,26 @@
+from django.forms import ValidationError
 from django.shortcuts import redirect, render
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
-from .models import GameCycle, Kikorik, Attractions
+from .models import GameCycle, Kikorik, Attractions, MakeTurn
+from .forms import MakeTurnForm
 # Other fuctions
 
+def checkGameFormForDuplicates(form):
+    if form.is_valid():
+        attractionsList = [
+            form['attractionOne'].value(),
+            form['attractionTwo'].value(),
+            form['attractionThree'].value(),
+            form['attractionFour'].value(),
+            form['attractionFive'].value()
+        ]
+        for i in attractionsList:
+            loopList = attractionsList
+            loopList.remove(i)
+            if i in loopList:
+                return False
+    return True
 
 # Create your views here.
 def baseTemplate(request):
@@ -24,8 +41,23 @@ def gamePage(request):
             return render(request, 'GameApp/game.html', context = context)
         kikorikToRender = Kikorik.objects.get(user = request.user)
         currentGameCycle = GameCycle.objects.filter(usersInGame = request.user)
+        attractionsInCurrentTurn = Attractions.objects.filter(gameDay = currentGameCycle[0].currentGameDay)
+        if request.method == 'POST':
+            makeTurnInst = MakeTurn(user=request.user, gameDay = currentGameCycle[0].currentGameDay)
+            form = MakeTurnForm(data=request.POST, instance=makeTurnInst)
+            if form.is_valid():
+                if checkGameFormForDuplicates(form) != True: 
+                    return redirect("GameAppGame")
+                kikorikToRender.isTurnMade = False
+                kikorikToRender.save()
+                form.save()
+                return redirect('GameAppGame')
+        else:
+            form = MakeTurnForm()
         context['kikorik'] = kikorikToRender
         context['currentGameCycle'] = currentGameCycle[0]
+        context['attractions'] = attractionsInCurrentTurn
+        context['attractionsForm'] = form
         return render(request, 'GameApp/game.html', context = context)
 
 def loginView(request):
@@ -45,3 +77,6 @@ def loginView(request):
 def logoutView(request):
     logout(request)
     return redirect('GameAppIndex')
+
+def adminPage(request):
+    return render(request, 'GameApp/base.html')
